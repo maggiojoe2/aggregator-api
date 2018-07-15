625c7e992fde470683bcf3558066b8a132fba6cf
Joe Maggio <joseph.maggio@viasat.com>
2018-07-14 23:12:33 -0500

    Added /notifications rout.

--- aggregator/aggregator.py
+++ aggregator/aggregator.py
@@ -4,9 +4,9 @@ import os
 
 import requests
 from sanic import Sanic
-from sanic.response import text
+from sanic.response import text, json
 
-from aggregator.db import save_to_db
+from aggregator.db import save_to_db, retrieve_objects_from_db
 from aggregator.log import logger, LOGGING_CONFIG_DEFAULTS
 
 
@@ -15,7 +15,7 @@ app.static('/test', 'docker-compose.yml')
 
 
 @app.route('/slack', methods=['POST'])
-def inbound_slack(request):
+async def inbound_slack(request):
     """inbound post messages from slack"""
     req = request.json
     logger.info(str(req))
@@ -24,15 +24,27 @@ def inbound_slack(request):
         return text(req['challenge'])
     elif req['type'] == 'event_callback':
         logger.info('Message recieved')
-        get_user = requests.post('https://slack.com/api/users.profile.get', data={'token': os.environ['SLACK_AUTH_TOKEN'], 'user': req['event']['user']})
+        get_user = requests.post('https://slack.com/api/users.profile.get', data={'token': str(os.environ['SLACK_AUTH_TOKEN']), 'user': req['event']['user']})
         noti_obj = {
             "msg": req['event']['text'],
             "from_program": "slack",
             "time_recieved": datetime.fromtimestamp(req['event_time']),
-            "sender_name": get_user.json()['profile']['display_name']
+            "sender_name": get_user.json()['profile']['display_name'],
+            "url": "https://aggregator-app.slack.com",
+            "read": False
         }
         logger.info('Notification Object: %s', noti_obj)
-        save_to_db(noti_obj)
+        await save_to_db(noti_obj)
         return text('Success')
 
     return text('Not Found', 404)
+
+
+@app.route('/notifications', methods=['GET'])
+async def get_notifications(_):
+    """Get notifications."""
+    query = {'read': False}
+    notifications = await retrieve_objects_from_db(query, 'email_db', 'email_collection', find_one=False)
+    noti_list = list(notifications)
+    logger.info(noti_list)
+    return json({"data": noti_list})
